a1e70dd6df1a1ee9c6520f84a443a54660a41057
Michał Dębski <debski.mi.zd@gmail.com>
2017-09-10 15:47:38 +0200

    Update dependencies part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,90 +2,91 @@
 
 module.exports = function(grunt) {
 
-  var serverTest = [
-      'jshint:server',
-      'jasmine_nodejs:server'
-  ];
+    var serverTest = [
+        'jshint:server',
+        'jasmine_nodejs:server'
+    ];
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['public/js/**/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    },
-    karma: {
-      unit: {
-        configFile: 'tests/karma_public.js',
-        background: true,
-        singleRun: false
-      },
-      single: {
-        configFile: 'tests/karma_public.js',
-        singleRun: true
-      }
-    },
-    jasmine_nodejs: {
-        options: {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['public/js/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
         },
-        server: {
-            specs: ['tests/server/**']
-        }
-    },
-    jshint: {
-      options: {
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      },
-      public: {
-          src: ['Gruntfile.js', 'public/js/**/*.js', 'tests/karma_public.js', 'tests/public/**/*.js']
-      },
-      server: {
-          src: ['Gruntfile.js', 'server/**/*.js', 'tests/server/**/*.js']
-      }
-    },
-    watch: {
-        public: {
-          files: ['<%= jshint.public.src %>'],
-          tasks: ['jshint:public', 'karma:unit:run']
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
         },
-        server: {
-          files: ['<%= jshint.server.src %>'],
-          tasks: serverTest
+        karma: {
+            unit: {
+                configFile: 'tests/karma_public.js',
+                background: true,
+                singleRun: false
+            },
+            single: {
+                configFile: 'tests/karma_public.js',
+                singleRun: true
+            }
+        },
+        jasmine_nodejs: {
+            options: {
+            },
+            server: {
+                specs: ['tests/server/**']
+            }
+        },
+        jshint: {
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                    document: true
+                },
+                esversion: 6
+            },
+            public: {
+                src: ['Gruntfile.js', 'public/js/**/*.js', 'tests/karma_public.js', 'tests/public/**/*.js']
+            },
+            server: {
+                src: ['Gruntfile.js', 'server/**/*.js', 'tests/server/**/*.js']
+            }
+        },
+        watch: {
+            public: {
+                files: ['<%= jshint.public.src %>'],
+                tasks: ['jshint:public', 'karma:unit:run']
+            },
+            server: {
+                files: ['<%= jshint.server.src %>'],
+                tasks: serverTest
+            }
         }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-jasmine-nodejs');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-jasmine-nodejs');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('build', ['jshint', 'karma', 'concat', 'uglify']);
+    grunt.registerTask('build', ['jshint', 'karma:single', 'concat', 'uglify']);
 
-  grunt.registerTask('watch_public_tests', ['karma:unit:start', 'watch:public']);
-  grunt.registerTask('watch_server_tests', ['watch:server']);
-  grunt.registerTask('public_test', ['jshint:public', 'karma:single']);
-  grunt.registerTask('server_test', serverTest);
+    grunt.registerTask('watch_public_tests', ['karma:unit:start', 'watch:public']);
+    grunt.registerTask('watch_server_tests', ['watch:server']);
+    grunt.registerTask('public_test', ['jshint:public', 'karma:single']);
+    grunt.registerTask('server_test', serverTest);
 
 };
--- tests/karma_public.js
+++ tests/karma_public.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'es6-shim'],
 
 
     // list of files / patterns to load in the browser
